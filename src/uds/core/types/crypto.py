0667fc1fd228bd9c93e32dd0e82411da3c916098
Adolfo Gómez García <dkmaster@dkmon.com>
2026-01-11 06:19:57 +0100

    Add comments to TunnelMaterial class attributes for clarity

--- src/uds/core/types/crypto.py
+++ src/uds/core/types/crypto.py
@@ -3,8 +3,8 @@ import dataclasses
 
 @dataclasses.dataclass
 class TunnelMaterial:
-    key_payload: bytes
-    key_send: bytes
-    key_receive: bytes
-    nonce_send: bytes
-    nonce_receive: bytes
+    key_payload: bytes    # 32 bytes
+    key_send: bytes       # 32 bytes
+    key_receive: bytes    # 32 bytes
+    nonce_send: bytes     # 12 bytes, used for payload and send to tunnel
+    nonce_receive: bytes  # 12 bytes
