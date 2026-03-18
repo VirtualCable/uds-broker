9d67bffe3276639b7de82b56a060bcc8ae77b622
Adolfo Gómez García <dkmaster@dkmon.com>
2026-03-18 15:18:15 +0100

    fix: Correct typo in TICKET_SECURED_OWNER constant name

--- src/uds/core/consts/ticket.py
+++ src/uds/core/consts/ticket.py
@@ -38,7 +38,7 @@ DEFAULT_TICKET_VALIDITY_TIME: typing.Final[int] = 60  # 1 minute
 MAX_TICKET_VALIDITY_TIME: typing.Final[int] = 60 * 60 * 24 * 7  # 1 week
 TUNNEL_TICKET_VALIDITY_TIME: typing.Final[int] = 60 * 60 * 24 * 7  # 1 week
 
-TICKET_SECURED_ONWER: typing.Final[str] = '#SECURE#'  # Just a "different" owner. If used anywhere, it's not important (will not fail), but weird enough
+TICKET_SECURED_OWNER: typing.Final[str] = '#SECURE#'  # Just a "different" owner. If used anywhere, it's not important (will not fail), but weird enough
 # The old comment about length of ticket, does not apply anymore, because the Owner has been moved to an own field
 TICKET_LENGTH: typing.Final[int] = 48  # Ticket length must much the length of the ticket length on tunnel server!!! (take care with previous note) - 
 
